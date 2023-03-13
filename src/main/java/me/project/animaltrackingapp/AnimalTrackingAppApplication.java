a4be26c85351296668e8eb6bbe99d37a99249b54
Николай <colt92@inbox.ru>
2023-03-13 21:31:40 +0500

    Initial commit

---
+++ src/main/java/me/project/animaltrackingapp/AnimalTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package me.project.animaltrackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalTrackingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/project/animaltrackingapp/AnimalTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.project.animaltrackingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
